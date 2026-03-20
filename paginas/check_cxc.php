79974cbb7bf4f165a22e5d97178feb8f9a6536c7
Salvatore Berticci <salvatoreberticci19@gmail.com>
2026-03-20 12:57:01 -0400

    Sincronización total: Incluir datos JSON, SQL y scripts anteriormente ignorados

---
+++ paginas/check_cxc.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'conexion.php';
+$res = $conn->query("DESCRIBE cuentas_por_cobrar");
+if (!$res) {
+    echo json_encode(["error" => $conn->error]);
+} else {
+    $cols = [];
+    while ($row = $res->fetch_assoc()) {
+        $cols[] = $row;
+    }
+    echo json_encode($cols);
+}
+?>
