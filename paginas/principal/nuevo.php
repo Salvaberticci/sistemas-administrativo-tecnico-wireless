50969aa66e903fa9af41c6d1a29bf2824e1a8f33
Salvatore Berticci <salvatoreberticci19@gmail.com>
2026-03-20 12:50:31 -0400

    Prueba de sincronización: Renombrar label Tipo de Conexión y unignore JSON data

--- paginas/principal/nuevo.php
+++ paginas/principal/nuevo.php
@@ -260,7 +260,7 @@ require_once '../includes/sidebar.php';
                     <div class="section-title">Información de Instalación y Pago</div>
 
                     <div class="col-md-6">
-                        <label for="tipo_conexion" class="form-label">Tipo de Conexión</label>
+                        <label for="tipo_conexion" class="form-label">Tipo de Conexión (TEST)</label>
                         <select name="tipo_conexion" id="tipo_conexion" class="form-select" required>
                             <option value="">-- Seleccione Conexión --</option>
                             <?php
