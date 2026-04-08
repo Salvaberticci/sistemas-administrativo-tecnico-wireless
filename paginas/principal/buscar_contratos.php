9c17cdab4891c5e02be5ab4135dc538bfa28d9aa
Salvatore Berticci <salvatoreberticci19@gmail.com>
2026-04-08 12:10:34 -0400

    Fix: Prioritize monthly payments in contract search history

--- paginas/principal/buscar_contratos.php
+++ paginas/principal/buscar_contratos.php
@@ -15,7 +15,7 @@ if (strlen($search_query) >= 3) {
                    p.nombre_plan, p.monto as monto_plan,
                    (SELECT COUNT(*) FROM contratos WHERE cedula = c.cedula) AS total_contratos,
                    (SELECT COUNT(*) FROM contratos WHERE cedula = c.cedula AND id <= c.id) AS nro_orden,
-                   (SELECT h.justificacion FROM cuentas_por_cobrar cxc LEFT JOIN cobros_manuales_historial h ON cxc.id_cobro = h.id_cobro_cxc WHERE cxc.id_contrato = c.id AND cxc.estado = 'PAGADO' AND h.justificacion IS NOT NULL ORDER BY cxc.fecha_pago DESC, cxc.id_cobro DESC LIMIT 1) as ultimo_justif
+                   (SELECT h.justificacion FROM cuentas_por_cobrar cxc LEFT JOIN cobros_manuales_historial h ON cxc.id_cobro = h.id_cobro_cxc WHERE cxc.id_contrato = c.id AND cxc.estado = 'PAGADO' AND h.justificacion REGEXP 'Enero|Febrero|Marzo|Abril|Mayo|Junio|Julio|Agosto|Septiembre|Octubre|Noviembre|Diciembre' ORDER BY cxc.fecha_pago DESC, cxc.id_cobro DESC LIMIT 1) as ultimo_justif
             FROM contratos c
             LEFT JOIN planes p ON c.id_plan = p.id_plan
             WHERE c.nombre_completo LIKE '%" . $search_query . "%' 
